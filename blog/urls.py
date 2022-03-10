ca57ce9e5a09c318f2083eb18ccf49bd367baf3b
richard.css <richardiwnl@outlook.com>
2022-03-09 23:16:13 -0300

    deploy

--- blog/urls.py
+++ blog/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
