69c49650bc7886c5e9fdf9cb59ae6b9746fd6b47
richard.css <richardiwnl@outlook.com>
2022-03-19 01:01:39 -0300

    Adding reCAPTCHA

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(zv&rgj^#&jzat+p$521t4r!37s6qxvp4qcp(k#0s03ir(oab#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,14 +1,35 @@
 from django.forms import ModelForm
 
 from .models import Comentario
+import requests
 
 
 class FormComentario(ModelForm):
     def clean(self):
-        data = self.cleaned_data
-        nome = data.get('nome_comentario')
-        email = data.get('email_comentario')
-        comentario = data.get('comentario')
+        raw_data = self.data
+
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+
+        cleaned_data = self.cleaned_data
+
+        nome = cleaned_data.get('nome_comentario')
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
+
+        recaptcha_request = requests.post('https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6Lf9kvEeAAAAALC0SwAxZ0cYenl1c-5OZiUCSK0e',
+                'response': recaptcha_response
+            }
+        )
+
+        recaptcha_result = recaptcha_request.json()
+
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comentario',
+                'Por favor, marque o campo "Não sou um robô".'
+            )
 
         if len(comentario) < 10:
             self.add_error('comentario', 'Comentário deve ter mais que 10 caracteres')
